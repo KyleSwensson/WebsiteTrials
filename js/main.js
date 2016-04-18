2a008f7a6e87df85cf2b14184c7048284ff81189
swooasd <kyle.swensson@gmail.com>
2016-04-18 02:11:27 -0700

    changed animations to jquery

--- js/main.js
+++ js/main.js
@@ -5,28 +5,37 @@ function myExpandLine(elemId, textElemId) {
 	var textPos = textElem.position();
 	var elem2 = $("#" + elemId);
 	clearInterval(id);
-	id = setInterval(frame, 5);
+	//id = setInterval(frame, 5);
 	var maxWidth = $("#"+textElemId).width();
 
 	elem2.css('top', textPos.top + (textElem.height() * 1.3));
 	elem2.css('left', textPos.left);
 
-	function frame() {
+	elem2.animate({
+		width: maxWidth
+	}, 'fast');
+
+/*	function frame() {
 		if (elem2.width() >= maxWidth) {
 		 clearInterval(id);	
 		} else {
 			elem2.width(elem2.width() + MOVESPEED);
 		}
-	}
+	}*/
 }
 
 
 function myShrinkLine(elemId) {
 	var elem = $("#" + elemId);
-	var width = 5;
+	var ewidth = 5;
 	var elemPos = elem.position();
-	clearInterval(id);
-	id = setInterval(frame,5);
+	var elemWidth = elem.width();
+	//clearInterval(id);
+	//id = setInterval(frame,5);
+	elem.animate({
+		width: 0,
+		left: (elemPos.left + elemWidth)
+	}, 'fast');
 	function frame() {
 		if (elem.width() <= 0) {
 			clearInterval(id);
